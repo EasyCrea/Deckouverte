9b8f51523cd4e4abaaf1fe16b3ff7d313f755408
Malo Caparros <malo.caparros@etu.univ-poitiers.fr>
2025-01-22 17:23:37 +0100

    modification css connexion

--- Deckouverte/app/page/Auth/login.js
+++ Deckouverte/app/page/Auth/login.js
@@ -12,6 +12,8 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { loginCreateur } from "../../fetch/Auth";
 import { useRouter } from "expo-router";
 import { buttonStyles } from "../../styles/buttons";
+import { FontAwesome } from '@expo/vector-icons';
+import { colors } from "../../styles/colors";
 
 import Svg, {
   Text as SvgText,
@@ -51,99 +53,83 @@ export function Login() {
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={styles.container}>
-        <Svg height="50" width="300" viewBox="0 0 300 50">
-          <Defs>
-            <LinearGradient id="gradient" x1="0" y1="0" x2="1" y2="0">
-              <Stop offset="0" stopColor="#6366f1" stopOpacity="1" />
-              <Stop offset="1" stopColor="#9333ea" stopOpacity="1" />
-            </LinearGradient>
-          </Defs>
-          <SvgText
-            fill="url(#gradient)"
-            fontSize="40"
-            fontWeight="800"
-            x="150"
-            y="40"
-            textAnchor="middle"
-          >
-            Connexion
-          </SvgText>
-        </Svg>
+      <View style={styles.title}>
+      <Svg height="50" width="300" viewBox="0 0 300 50">
+        <Defs>
+          <LinearGradient id="gradient" x1="0" y1="0" x2="1" y2="0">
+            <Stop offset="0" stopColor="#6366f1" stopOpacity="1" />
+            <Stop offset="1" stopColor="#9333ea" stopOpacity="1" />
+          </LinearGradient>
+        </Defs>
+        <SvgText
+          fill="url(#gradient)"
+          fontSize="40"
+          fontWeight="800"
+          x="150"
+          y="40"
+          textAnchor="middle"
+        >
+          Connexion
+        </SvgText>
+      </Svg>
+      </View>
+      <View style={styles.btnBackbox}>
+      <Pressable 
+        style={buttonStyles.btnBack}
+        onPress={() => router.back()}
+      >
+        <FontAwesome name="arrow-left" size={18} color="#333333" />
+      </Pressable>
+      </View>
 
-        <View style={styles.formSection}>
-          <View style={styles.inputGroup}>
-            <Text style={styles.label}>Email</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Votre email"
-              placeholderTextColor="#666666"
-              value={email}
-              onChangeText={setEmail}
-              keyboardType="email-address"
-              autoCapitalize="none"
-              autoCorrect={false}
-              required
-            />
-          </View>
-
-          <View style={styles.inputGroup}>
-            <Text style={styles.label}>Mot de passe</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Votre mot de passe"
-              placeholderTextColor="#666666"
-              value={password}
-              onChangeText={setPassword}
-              secureTextEntry
-              required
-            />
-          </View>
-
-          {error ? <Text style={styles.error}>{error}</Text> : null}
-
-          <Pressable
-            style={({ pressed }) => [
-              buttonStyles.btn,
-              buttonStyles.btnFilled,
-              buttonStyles.btnText,
-              pressed && buttonStyles.btnFilledPressed,
-              loading && buttonStyles.buttonDisabled,
-            ]}
-            onPress={handleLogin}
-            disabled={loading}
-          >
-            {loading ? (
-              <ActivityIndicator color="#FFFFFF" />
-            ) : (
-              <Text style={styles.buttonText}>Se connecter</Text>
-            )}
-          </Pressable>
+      <View style={styles.formSection}>
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Votre email"
+            placeholderTextColor="#666666"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            required
+          />
         </View>
 
-        <View style={styles.footerSection}>
-          <Pressable
-            onPress={() =>
-              router.push({
-                pathname: "/page/Auth/userconnexion",
-                params: { page: "register" },
-              })
-            }
-          >
-            <Text style={{ color: "blue" }}>
-              Pas encore de compte ? Inscrivez-vous
-            </Text>
-          </Pressable>
-          <Pressable
-            onPress={() =>
-              router.push({
-                pathname: "/",
-              })
-            }
-          >
-            <Text style={{ color: "blue" }}>Retour à la page d'accueil</Text>
-          </Pressable>
+        <View style={styles.inputGroup}>
+          <Text style={styles.label}>Mot de passe</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Votre mot de passe"
+            placeholderTextColor="#666666"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+            required
+          />
         </View>
+
+        {error ? <Text style={styles.error}>{error}</Text> : null}
+
+        <Pressable
+          style={({ pressed }) => [
+            buttonStyles.btn,
+            buttonStyles.btnFilled,
+            buttonStyles.btnText,
+            pressed && buttonStyles.btnFilledPressed,
+            loading && buttonStyles.buttonDisabled,
+          ]}
+          onPress={handleLogin}
+          disabled={loading}
+        >
+          {loading ? (
+            <ActivityIndicator color="#FFFFFF" />
+          ) : (
+            <Text style={styles.buttonText}>Se connecter</Text>
+          )}
+        </Pressable>
       </View>
     </SafeAreaView>
   );
@@ -153,21 +139,22 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: "#f8f7ff",
-  },
-  container: {
-    flex: 1,
     padding: 20,
-    justifyContent: "space-between",
     alignItems: "center",
     fontFamily: "arial",
   },
-  footerSection: {
-    alignItems: "center",
-    marginBottom: 20,
+  title: {
+    marginBottom: 80,
   },
+  btnBackbox: {
+    marginBottom: 20,
+    width: "80%",
+    alignItems: "flex-start",
+  },
+
   formSection: {
-    width: "100%",
-    padding: 10,
+    justifyContent: "center",
+    width: "80%",
   },
   inputGroup: {
     marginBottom: 20,
@@ -176,7 +163,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: "600",
     marginBottom: 8,
-    color: "#333333",
+    color: colors.indigo700,
     paddingLeft: 4,
   },
   input: {
@@ -187,7 +174,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: "#333333",
     borderWidth: 1,
-    borderColor: "#E0E0E0",
+    borderColor: colors.indigo200,
   },
   error: {
     color: "#FF4D4D",
