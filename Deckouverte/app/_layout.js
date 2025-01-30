cfdfd107f735c9399b59b70a5c516bb0245a0f1d
Malo Caparros <malo.caparros@etu.univ-poitiers.fr>
2025-01-30 17:13:30 +0100

    modif pour faire fonctionner le jeu

--- Deckouverte/app/_layout.js
+++ Deckouverte/app/_layout.js
@@ -1,11 +1,14 @@
 import { Slot } from 'expo-router';
 import { View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import "./global.css";
 
 export default function Layout() {
   return (
-    <View style={{ flex: 1 }}>
-      <Slot />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
+        <Slot />
+      </View>
+    </GestureHandlerRootView>
   );
 }
